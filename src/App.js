203f6814570eaa4fc6bad793f52a5595db73c057
Zehra.M <139117868+MemmedzadeZ@users.noreply.github.com>
2024-12-25 18:03:31 +0400

    commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -15,7 +15,8 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          deploy react application to firebase using github actions (Zehra
+          Memmedzade)
         </a>
       </header>
     </div>
